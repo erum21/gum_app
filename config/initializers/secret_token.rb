5539035d6058e34d223bf8e958331347d9a525d3
Edwin Vega <edwin.ponc@gmail.com>
2013-05-04 20:57:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GumApp::Application.config.secret_token = 'a39824c225e10ee55479e034e3731fee22b71857601fe762902a5e07549b396d92945c67fc8bb4e406c08178eb73af2e5ae0de7bb4f87780771db7dd56913b81'
